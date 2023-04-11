8c575bf118142ddfe279962d5a552ebb4eb3af91
T1 <126980610+tadash10@users.noreply.github.com>
2023-04-10 23:39:13 -0300

    Update and rename beta.2.py to beta3.py

--- beta3.py
+++ beta3.py
@@ -1,27 +1,39 @@
 import pytest
 import os
 
-# DISCLAIMER: This script is provided "as is" and without warranty of any kind. 
+# DISCLAIMER: This script is provided "as is" and without warranty of any kind.
 # Use at your own risk. The author shall not be liable for any damages arising from the use of this script.
 
+
 def run_tests():
-    # This function runs a suite of tests using pytest.
+    """
+    This function runs a suite of tests using pytest.
+    """
     os.chdir('tests')
     pytest.main(['--verbose'])
     os.chdir('..')
 
+
 def check_coverage():
-    # This function checks test coverage using pytest-cov.
+    """
+    This function checks test coverage using pytest-cov.
+    """
     os.chdir('tests')
     pytest.main(['--cov=app', '--cov-report=term-missing', '--verbose'])
     os.chdir('..')
 
+
 def run_linters():
-    # This function runs linters using flake8.
+    """
+    This function runs linters using flake8.
+    """
     os.system('flake8 app tests')
 
+
 def menu():
-    # This function displays the menu and allows the user to choose which option to execute.
+    """
+    This function displays the menu and allows the user to choose which option to execute.
+    """
     while True:
         print('What would you like to do?')
         print('1. Run tests')
@@ -40,5 +52,7 @@ def menu():
         else:
             print('Invalid choice')
 
+
 if __name__ == '__main__':
+    print("Welcome to the testing and linting script, by T1!\n")
     menu()
